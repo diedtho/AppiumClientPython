d1f7d164015c63b7d9ad24bfe3a320b9f6e2a7b3
diedtho <diederichst@bfwob.de>
2022-09-08 08:24:33 +0200

    Dataclasses extended

--- appium_webelement_button2class.py
+++ appium_webelement_button2class.py
@@ -5,7 +5,7 @@ from appium import webdriver
 from pprint import pprint
 from bs4 import BeautifulSoup
 
-from powerpoint_elements import attrs2button
+from powerpoint_elements import attrs2buttons, attrs2tab_items, attrs2menu_items
 from operator import attrgetter
 
 desired_caps = {}
@@ -23,25 +23,43 @@ elements = session.find_elements_by_xpath('/descendant-or-self::*')
 pprint(elements)
 print('='*222)
 
+for elem in elements:
+    print(elem.tag_name, elem.text)
+
 # Buttons
 buttons = []
+menu_items = []
+tab_items = []
 
 for i in range(len(elements)):
     tagname = elements[i].tag_name
     if str(tagname) in 'ControlType.Button':
-        button = attrs2button(elements[i], i, "unnamed")
+        button = attrs2buttons(elements[i], i, "unnamed")
         buttons.append(button)
-
+    if str(tagname) in 'ControlType.TabItem':
+        tab_item = attrs2tab_items(elements[i], i, "unnamed")
+        tab_items.append(tab_item)
+    if str(tagname) in 'ControlType.MenuItem':
+        menu_item = attrs2menu_items(elements[i], i, "unnamed")
+        menu_items.append(menu_item)
 
 pprint(buttons)
+pprint(tab_items)
+pprint(menu_items)
 
 print("\nÖffnen-Buttons")
 text_getter = attrgetter('text')
 #oldest = max(buttons, key=text_getter)
 #print(f"Oldest: {oldest}")
-oeffnen = [button for button in buttons if button.text == 'Öffnen']
-pprint(oeffnen)
+start = [tab_item for tab_item in tab_items if tab_item.text == 'Start']
+pprint(start)
 
-oeffnen[0].webelement.click()
+start[0].webelement.click()
+start[0].webelement.click()
+
+datei = [button for button in buttons if button.text == 'Registerkarte "Datei"']
+datei[0].click()
+datei[0].click()
+
 
 driver.close()
--- powerpoint_elements.py
+++ powerpoint_elements.py
@@ -10,9 +10,38 @@ class Button:
     parent: str
     button_id: str
 
-def attrs2button(webele, i, parent):
+@dataclass
+class TabItem:
+    name: str
+    number: int
+    text: str
+    webelement: WebElement
+    parent: str
+    button_id: str
+
+@dataclass
+class MenuItem:
+    name: str
+    number: int
+    text: str
+    webelement: WebElement
+    parent: str
+    button_id: str
+
+def attrs2menu_items(webele, i, parent):
+    elemid = webele.id
+    elemtext = webele.text
+    menu_item = MenuItem(name="unnamed", number=i, text=elemtext, webelement=webele, button_id=elemid, parent=parent)
+    return menu_item
+
+def attrs2tab_items(webele, i, parent):
+    elemid = webele.id
+    elemtext = webele.text
+    tab_item = TabItem(name="unnamed", number=i, text=elemtext, webelement=webele, button_id=elemid, parent=parent)
+    return tab_item
+
+def attrs2buttons(webele, i, parent):
     elemid = webele.id
     elemtext = webele.text
     button = Button(name="unnamed", number=i, text=elemtext, webelement=webele, button_id=elemid, parent=parent)
     return button
-
